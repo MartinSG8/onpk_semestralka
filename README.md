685dce6559c7fffd5146bbb88bb8f722a3bc25d2
MartinSG8 <matonimohaj@gmail.com>
2023-11-27 19:04:11 +0100

    Add app source code.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Dokumentácia
+
+## Časť 1.: Terraform
+
+Pomocou terraformu (Nástroj IaC) vytvoríme infraštruktúru s ktorou budeme ďalej pracovať.
--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -7,8 +7,9 @@ terraform {
   }
 }
 
-resource "openstack_networking_network_v2" "network_1" {
-  name           = "network_1"
+# --- <network&subnet> ---
+resource "openstack_networking_network_v2" "private_network_1" {
+  name           = "private_network_1"
   admin_state_up = "true"
 }
 
@@ -18,10 +19,36 @@ resource "openstack_networking_subnet_v2" "subnet_1" {
   cidr       = "192.168.1.0/24"
   ip_version = 4
 }
+# --- <!network&subnet!> ---
 
-resource "openstack_networking_subnet_v2" "subnet_2" {
-  name       = "subnet_2"
-  network_id = openstack_networking_network_v2.network_1.id
-  cidr       = "192.168.2.0/24"
-  ip_version = 4
+# --- <security_group&security_group_rule> ---
+resource "openstack_networking_secgroup_v2" "public_secgroup_1" {
+  name        = "public_secgroup_1"
+  description = "Security group for public instance"
 }
+
+resource "openstack_networking_secgroup_rule_v2" "public_secgroup_rule_1" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_1.id
+}
+
+resource "openstack_networking_secgroup_v2" "private_secgroup_1" {
+  name        = "private_secgroup_1"
+  description = "Security group for public instance"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "private_secgroup_rule_1" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_1.id
+}
+# --- <!security_group&security_group_rule!> ---
